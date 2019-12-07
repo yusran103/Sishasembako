2b74af1cb4a248a0c36c1926639842f81808ba13
Yusran Ihza Mahendra <edo.hunt312@gmail.com>
2019-12-08 00:13:29 +0700

    login tampilan admin pasar, halaman pengunjung

--- pasar/urls.py
+++ pasar/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sishasembapo import views as pasar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', pasar.Login, name="login"),
+    path('', pasar.index, name="index"),
+    path('logout', pasar.Logout, name="logout")
 ]
--- sishasembapo/views.py
+++ sishasembapo/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 ,redirect
+from django.contrib import messages, auth
+from django.contrib.auth import authenticate, logout, login
+from sishasembapo.models import *
 
 # Create your views here.
+def Login(request):
+    if request.POST:
+        user = authenticate(username=request.POST['login_username'], password=request.POST['login_password'])
+        if user is not None:
+            if not user.is_staff:
+                try:
+                    profil = PetugasPasar.objects.get(akun_id=user.id)
+                    login(request, user)
+                    request.session['nama'] = profil.nama
+                    request.session['pasar'] = profil.pasar.nama
+                    for key, value in request.session.items():
+                        print('{} => {}'.format(key, value))
+                    return redirect('/')
+                except PetugasPasar.DoesNotExist:
+                    messages.add_message(request, messages.INFO, 'Akun Tersebut Belum Terintegrasi dengan Petugas Pasar, Silahkan Hubungi Admin PD. Pasar')  
+            else:
+               messages.add_message(request, messages.INFO, 'Akun Tersebut Bukan Termasuk Akun Petugas Pasar') 
+        else:
+            messages.add_message(request, messages.INFO, 'Username atau password Anda salah')
+    return render(request, 'login.html')
+
+def index(request):
+    for key, value in request.session.items():
+        print('{} => {}'.format(key, value))
+    return render(request, 'pengunjung/index.html')
+
+def Logout(request):
+    logout(request)
+    for key in request.session.keys():
+        del request.session[key]
+    return redirect('/')
